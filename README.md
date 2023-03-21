b55929393ab5349356c648ef42ec35ac280c9833
kfzz1 <spaced0g@gov.com>
2023-03-21 11:07:34 +0800

    update

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 - 支持Telegram
 - 支持IPv6
 - 增加域名解析
+- 目前仅支持[dynv6](https://dynv6.com/)一家DDNS平台
+- 每日早上8点、下午2点和晚上9点自动检测DDNS状态，配合[DDNS-GO](https://github.com/jeessy2/ddns-go)使用
+
+>如果OpenWRT遇到时间不对的问题，参考[https://github.com/jeessy2/ddns-go/issues/497](https://github.com/jeessy2/ddns-go/issues/497)，安装`zoneinfo-asia`并重启OpenWRT。
 
 需要添加[DDNSBOT](https://t.me/KDDNS_bot)
 
--- main.go
+++ main.go
@@ -92,38 +92,67 @@ func sendUrl(url, method string, data map[string]interface{}) string {
 	return ""
 }
 
+func webHook(public_ip, domain, text string, ip_strings []string) {
+	data := map[string]interface{}{
+		"ipv6": map[string]string{
+			// "text":    "Detect the current server's DDNS status: ",
+			"text":    text,
+			"result":  "Success",
+			"addr":    public_ip,
+			"domains": domain,
+			"resolve": strings.Join(ip_strings, ","),
+		},
+	}
+	DDNS_bot_webhook := sendUrl("<DDNS_BOT_WEBHOOK>", "POST", data)
+	fmt.Println(DDNS_bot_webhook)
+}
+
 func main() {
+
 	fmt.Println("let's start the program: ")
-	// 在每天的早上八点，下午两点和晚上九点检测DDNS是否可用
-	ticker := time.NewTicker(24 * time.Hour)
-	for range ticker.C {
+	for {
 		now := time.Now()
-		if now.Hour() == 8 || now.Hour() == 14 || now.Hour() == 21 {
-			fmt.Println("It's time to execute the scheduled task!")
-			// 在此处编写需要执行的任务代码
-			domain := "DOMAIN"
-			// 解析IP地址
-			ips, err := resolveIPs(domain)
-			if err != nil {
-				panic(err)
+		year, month, day := now.Date()
+		targetTimes := []time.Time{
+			time.Date(year, month, day, 8, 0, 0, 0, now.Location()),
+			time.Date(year, month, day, 14, 0, 0, 0, now.Location()),
+			time.Date(year, month, day, 21, 0, 0, 0, now.Location()),
+		}
+
+		var targetTime time.Time
+		for _, t := range targetTimes {
+			if !now.After(t) { // 找到下一个目标时间
+				targetTime = t
+				break
 			}
-			var ip_strings []string
-			for _, ip := range ips {
-				ip_strings = append(ip_strings, ip.String())
-			}
-			public_ip := sendUrl("http://6.ipw.cn", "GET", nil)
-			data := map[string]interface{}{
-				"ipv6": map[string]string{
-					"text":    "Detect the current server's DDNS status: ",
-					"result":  "Success",
-					"addr":    public_ip,
-					"domains": domain,
-					"resolve": strings.Join(ip_strings, ","),
-				},
-			}
-			DDNS_bot_webhook := sendUrl("DDNS_BOT_WEBHOOK", "POST", data)
-			fmt.Println(DDNS_bot_webhook)
+		}
+
+		duration := targetTime.Sub(now)
+		timer := time.NewTimer(duration)
+
+		<-timer.C
+		fmt.Println("It's time to execute the scheduled task!")
+		// 在此处编写需要执行的任务代码
+		domain := "<DOMAIN>"
+		// 解析IP地址
+		ips, err := resolveIPs(domain)
+		if err != nil {
+			panic(err)
+		}
+		var ip_strings []string
+		for _, ip := range ips {
+			ip_strings = append(ip_strings, ip.String())
+		}
+		public_ip := sendUrl("http://6.ipw.cn", "GET", nil)
+		if public_ip != ip_strings[0] {
+			text := "The public network ip is inconsistent with the ip of domain name resolution: "
+			webHook(public_ip, domain, text, ip_strings)
+			update_domain_url := "http://dynv6.com/api/update?hostname=" + domain + "&token=<dynv6_USERNAME>=" + public_ip + "&ipv6prefix="
+			result := sendUrl(update_domain_url, "GET", nil)
+			fmt.Println(result)
+		} else {
+			text := "Detect the current server's DDNS status:"
+			webHook(public_ip, domain, text, ip_strings)
 		}
 	}
-
 }
