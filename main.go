11768d1bcfe384cb2be4c8e9655f64ad0065d122
kfzz1 <spaced0g@gov.com>
2023-03-21 22:43:45 +0800

    update code

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 func resolveIPs(domain string) ([]net.IP, error) {
@@ -109,28 +111,13 @@ func webHook(public_ip, domain, text string, ip_strings []string) {
 
 func main() {
 
-	fmt.Println("let's start the program: ")
-	for {
-		now := time.Now()
-		year, month, day := now.Date()
-		targetTimes := []time.Time{
-			time.Date(year, month, day, 8, 0, 0, 0, now.Location()),
-			time.Date(year, month, day, 14, 0, 0, 0, now.Location()),
-			time.Date(year, month, day, 21, 0, 0, 0, now.Location()),
-		}
-
-		var targetTime time.Time
-		for _, t := range targetTimes {
-			if !now.After(t) { // 找到下一个目标时间
-				targetTime = t
-				break
-			}
-		}
-
-		duration := targetTime.Sub(now)
-		timer := time.NewTimer(duration)
-
-		<-timer.C
+	// 新建一个定时任务对象
+	// 根据cron表达式进行时间调度，cron可以精确到秒，大部分表达式格式也是从秒开始。
+	//crontab := cron.New()  默认从分开始进行时间调度
+	crontab := cron.New(cron.WithSeconds()) //精确到秒
+	//定义定时器调用的任务函数
+	task := func() {
+		fmt.Println("hello world", time.Now())
 		fmt.Println("It's time to execute the scheduled task!")
 		// 在此处编写需要执行的任务代码
 		domain := "<DOMAIN>"
@@ -147,7 +134,7 @@ func main() {
 		if public_ip != ip_strings[0] {
 			text := "The public network ip is inconsistent with the ip of domain name resolution: "
 			webHook(public_ip, domain, text, ip_strings)
-			update_domain_url := "http://dynv6.com/api/update?hostname=" + domain + "&token=<dynv6_USERNAME>=" + public_ip + "&ipv6prefix="
+			update_domain_url := "http://dynv6.com/api/update?hostname=" + domain + "&token=<dynv6_TOKEN>=" + public_ip + "&ipv6prefix="
 			result := sendUrl(update_domain_url, "GET", nil)
 			fmt.Println(result)
 		} else {
@@ -155,4 +142,14 @@ func main() {
 			webHook(public_ip, domain, text, ip_strings)
 		}
 	}
+	//定时任务
+	spec := "0 0 8,14,21,0 * * ?"
+	// 添加定时任务,
+	crontab.AddFunc(spec, task)
+	// 启动定时器
+	crontab.Start()
+	// 定时任务是另起协程执行的,这里使用 select 简答阻塞.实际开发中需要
+	// 根据实际情况进行控制
+	select {} //阻塞主线程停止
+
 }
